d66eb0c6694f4014c4bee36c6da9493dfaed2036
Arnaud <58701505+arnaud-roudiere@users.noreply.github.com>
2022-04-12 18:13:44 +0200

    Update tournoi.py

--- models/tournoi.py
+++ models/tournoi.py
@@ -1,36 +1,29 @@
-""" Define tournament """
-
 from datetime import datetime
 
 
 class Tournament:
     def __init__(
             self,
-            name: str = None,
-            location: str = None,
-            tournament_date: datetime = None,
-            time_type: str = None,
-            no_of_players: int = 8,
-            no_of_rounds: int = 4
-    ):
-        self.name = str(name)
-        self.location = str(location)
-        self.tournament_date = str(tournament_date)
-        self.time_type = str(time_type)
-        self.no_of_players = no_of_players
-        self.no_of_rounds: no_of_rounds
-        """
-        self.players: List[Player] = []
-        self.players_ids = []
-        self.description = None
-        self.no_of_players = no_of_players
-        self.playing = False
-        self.finished = False
-        """
-    def __str__(self):
-        """ Str print """
-        return self.name
+            nom_du_tournoi: str = None,
+            lieu: str = None,
+            date_debut: datetime = None,
+            date_fin: datetime = None,
+            joueurs: int = 8,
+            nombre_de_tours: int = 4,
+            tournees: int = 4,
+            controle_du_temps: str = None,
+            description: str = None
 
-    def __repr__(self):
-        """Used in print."""
-        return str(self)
+    ):
+        self.nom_du_tournoi = str(nom_du_tournoi)
+        self.lieu = str(lieu)
+        self.date_debut = str(date_debut)
+        self.date_fin = str(date_fin)
+        self.joueurs = joueurs
+        self.nombre_de_tours: nombre_de_tours
+        self.tournees = tournees
+        self.controle_du_temps = controle_du_temps
+        self.description = description
+
+    def __str__(self):
+        return self.nom_du_tournoi
